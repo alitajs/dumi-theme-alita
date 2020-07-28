e97cdb8603cd56858080156f9cd88ab97a35745d
chenshuhang <437132124@qq.com>
2020-07-28 21:09:25 +0800

    chore: button component insert round

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -1,20 +1,37 @@
 import React, { FC } from 'react';
+import classnames from 'classnames';
 import './index.less';
 
 interface ButtonProps {
   href: string;
+  round?: boolean;
 }
 
-const Button: FC<ButtonProps> = ({ children, href }) => {
+const Button: FC<ButtonProps> = ({ children, href, round = false }) => {
   if (typeof href === 'string') {
     return (
-      <a href={href} className="dumi-alita-button-a">
+      <a
+        href={href}
+        className={classnames({
+          'dumi-alita-button-a': true,
+          'dumi-alita-button-round': round,
+        })}
+      >
         {children}
       </a>
     );
   }
 
-  return <div className="dumi-alita-button">{children}</div>;
+  return (
+    <div
+      className={classnames({
+        'dumi-alita-button': true,
+        'dumi-alita-button-round': round,
+      })}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Button;
