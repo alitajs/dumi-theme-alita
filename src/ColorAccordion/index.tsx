cb2daa8d8ab76c289933896ce9ff50f438235aab
chenshuhang <437132124@qq.com>
2020-07-29 09:48:25 +0800

    fix: color-accordion change insert field

--- src/ColorAccordion/index.tsx
+++ src/ColorAccordion/index.tsx
@@ -1,9 +1,9 @@
 import React, { FC, useState } from 'react';
 import classnames from 'classnames';
-import { colors } from './color';
+import { ColorItemProps, ColorAccordionProps } from './interface';
 import './index.less';
 
-const ColorAccordion: FC = () => {
+const ColorAccordion: FC<ColorAccordionProps> = ({ colors = [] }) => {
   const [activeColor, setActiveColor] = useState<string>('');
 
   const toggleActiveColor = (color: string) => {
@@ -17,28 +17,27 @@ const ColorAccordion: FC = () => {
   return (
     <div className="dumi-alita-color-accordion">
       <ul className="dumi-alita-color-menu">
-        {Object.keys(colors).map(color => {
-          const isActive = activeColor === color ? true : false;
+        {colors.map((item: ColorItemProps, index: number) => {
+          const { name, color, contrast, shade = '', tint = '' } = item;
+          const isActive = activeColor === name ? true : false;
           return (
             <li
-              key={color}
+              key={`${name}-${index}`}
               className={classnames({
                 'dumi-alita-color-menu-item': true,
                 'dumi-alita-color-menu-item-active': isActive,
               })}
               style={{
-                backgroundColor: colors[color][`dumi-alita-color-${color}`],
-                color: colors[color][`dumi-alita-color-${color}-contrast`],
+                backgroundColor: color,
+                color: contrast,
               }}
             >
               <div
                 className="dumi-alita-color-menu-text"
-                onClick={() => toggleActiveColor(color)}
+                onClick={() => toggleActiveColor(name)}
               >
                 {color[0].toUpperCase() + color.substr(1)}
-                <div className="dumi-alita-color-menu-value">
-                  {colors[color][`dumi-alita-color-${color}`]}
-                </div>
+                <div className="dumi-alita-color-menu-value">{color}</div>
               </div>
 
               <svg
@@ -72,36 +71,34 @@ const ColorAccordion: FC = () => {
                 </g>
               </svg>
               <ul className="dumi-alita-color-submenu">
-                <li
-                  className="dumi-alita-color-submenu-item"
-                  style={{
-                    backgroundColor:
-                      colors[color][`dumi-alita-color-${color}-shade`],
-                    color: colors[color][`dumi-alita-color-${color}-contrast`],
-                  }}
-                >
-                  <div className="dumi-alita-color-menu-text">
-                    Shade
-                    <div className="dumi-alita-color-menu-value">
-                      {colors[color][`dumi-alita-color-${color}-shade`]}
+                {shade && (
+                  <li
+                    className="dumi-alita-color-submenu-item"
+                    style={{
+                      backgroundColor: shade,
+                      color: contrast,
+                    }}
+                  >
+                    <div className="dumi-alita-color-menu-text">
+                      Shade
+                      <div className="dumi-alita-color-menu-value">{shade}</div>
                     </div>
-                  </div>
-                </li>
-                <li
-                  className="dumi-alita-color-submenu-item"
-                  style={{
-                    backgroundColor:
-                      colors[color][`dumi-alita-color-${color}-tint`],
-                    color: colors[color][`dumi-alita-color-${color}-contrast`],
-                  }}
-                >
-                  <div className="dumi-alita-color-menu-text">
-                    Tint
-                    <div className="dumi-alita-color-menu-value">
-                      {colors[color][`dumi-alita-color-${color}-tint`]}
+                  </li>
+                )}
+                {tint && (
+                  <li
+                    className="dumi-alita-color-submenu-item"
+                    style={{
+                      backgroundColor: tint,
+                      color: contrast,
+                    }}
+                  >
+                    <div className="dumi-alita-color-menu-text">
+                      Tint
+                      <div className="dumi-alita-color-menu-value">{tint}</div>
                     </div>
-                  </div>
-                </li>
+                  </li>
+                )}
               </ul>
             </li>
           );
